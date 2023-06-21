1914d87d9c922ba4e4f477bf9af1ebded37da105
babu ankaian <babu.ankaian0311@gmail.com>
2023-06-21 16:43:45 +0530

    config.js removed on .gitignore file and now pushing config js to repository

---
+++ components/config.js
@@ -0,0 +1,9 @@
+// All environment variables will be sourced
+// Do NOT commit this file to source control
+module.exports = {
+space_id: 'y5mc1tp2v6n4',
+delivery_token: 'b_ErXnEyah92BhfmFsxAJRajkM-WmPxpf_7c6SuEYAM',
+preview_token: 'dBO-HykyH2SsnuTQmd9QrIrVUDZIq0PYwDoKDgltJ8w',
+previewSecret: 'bicsglobal',
+environment: 'master'
+}
